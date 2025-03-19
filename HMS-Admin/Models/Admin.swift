b7aacbf96ebd46b9b5d63bd7e235f58b2e287a76
Ritik Ranjan <ritik0ranjan+github@gmail.com>
2025-03-19 14:32:03 +0530

    Add base model & api controller class

---
+++ HMS-Admin/Models/Admin.swift
@@ -0,0 +1,26 @@
+//
+//  Admin.swift
+//  HMS-Admin
+//
+//  Created by RITIK RANJAN on 19/03/25.
+//
+
+import Foundation
+
+enum Role: String, Codable {
+    case admin
+}
+
+struct Admin: Codable, Equatable {
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case emailAddress = "email_address"
+        case password = "password"
+        case role = "role"
+    }
+
+    let id: String = UUID().uuidString
+    let emailAddress: String
+    let password: String
+    let role: Role = .admin
+}
