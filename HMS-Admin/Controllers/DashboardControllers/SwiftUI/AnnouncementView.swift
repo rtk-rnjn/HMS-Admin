18b1c8ade83b94f9940e0a68f844b53bf4b49e17
Dhruvi <dhruvityagi04@gmail.com>
2025-04-02 16:07:27 +0530

    push presentation & in line title

--- HMS-Admin/Controllers/DashboardControllers/SwiftUI/AnnouncementView.swift
+++ HMS-Admin/Controllers/DashboardControllers/SwiftUI/AnnouncementView.swift
@@ -14,7 +14,6 @@ struct AnnouncementView: View {
     var announcements: [Announcement] = []
 
     var body: some View {
-        NavigationView {
             List(announcements, id: \.title) { announcement in
                 HStack(spacing: 12) {
                     categoryIcon(for: announcement.category)
@@ -33,15 +32,6 @@ struct AnnouncementView: View {
                     }
                 }
                 .padding(4)
-            }
-            .navigationTitle("Notifications")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("Close") {
-                        dismiss()
-                    }
-                }
-            }
         }
     }
 
